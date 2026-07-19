81877ea46ec64bba664d6228d7316fb73dd9a86f
wangliaoqiongchen <3078575886@qq.com>
2026-07-19 17:06:29 +0800

    1.n

--- HelloWorld/src/Main.kt
+++ HelloWorld/src/Main.kt
@@ -1,4 +1,18 @@
 fun main() {
-    val student = Student("小明" , 18)
-    student.hello("小强")
+    val str: String = "Hello World"
+    println(str.length)
+    //     获取字符串长度
+    val str2: String = "10"
+    println(str2.toInt())
+    //可以用toInt把字符串10转换成数字10
+    val str3: String = "Hello World"
+    println(str3.uppercase())
+    //uppercase转换成大写
+    val str4: String = "Hello World"
+    println(str4.lowercase())
+    //转换成小写
+    val str5: String = "Hello World"
+    println(str5.replace("o", "a"))
+    //把o替换成a
+
 }
